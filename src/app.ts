4b0ccfb4304c4f709b61fd9df91b635b68da2c02
Sascha Nowak <sascha.nowak@netlogix.de>
2016-01-04 11:12:16 +0100

    [TASK] Add basic footer

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import {FORM_PROVIDERS, COMMON_DIRECTIVES} from 'angular2/common';
 import {PageComponent} from './components/page/page';
 import {NavigationComponent} from './components/navigation/navigation';
 import {OffcanvasComponent} from './components/navigation/offcanvas';
+import {FooterComponent} from './components/footer/footer';
 import {LoginComponent} from './components/login/login';
 import {AppStates} from './providers/appStates';
 import {States} from './model/states';
@@ -11,7 +12,7 @@ import {States} from './model/states';
 @Component({
   selector: 't3dd16-app',
   providers: [...FORM_PROVIDERS],
-  directives: [...ROUTER_DIRECTIVES, ...COMMON_DIRECTIVES, NavigationComponent, OffcanvasComponent, LoginComponent],
+  directives: [...ROUTER_DIRECTIVES, ...COMMON_DIRECTIVES, NavigationComponent, OffcanvasComponent, LoginComponent, FooterComponent],
   pipes: [],
   host: {},
   styleUrls: [
