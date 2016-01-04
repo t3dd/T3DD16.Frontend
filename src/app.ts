fab86a4ad7798330aaf0c5e38a00456d6f7b08ea
Sascha Nowak <sascha.nowak@netlogix.de>
2016-01-04 14:40:33 +0100

    [TASK] Add initial loading and embed svg

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,25 @@ import {Component, ViewEncapsulation} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS, COMMON_DIRECTIVES} from 'angular2/common';
 import {PageComponent} from './components/page/page';
+
 import {NavigationComponent} from './components/navigation/navigation';
 import {OffcanvasComponent} from './components/navigation/offcanvas';
 import {FooterComponent} from './components/footer/footer';
 import {LoginComponent} from './components/login/login';
+import {LoadingComponent} from './components/loading/loading';
+
 import {AppStates} from './providers/appStates';
 import {States} from './model/states';
 
 @Component({
   selector: 't3dd16-app',
   providers: [...FORM_PROVIDERS],
-  directives: [...ROUTER_DIRECTIVES, ...COMMON_DIRECTIVES, NavigationComponent, OffcanvasComponent, LoginComponent, FooterComponent],
+  directives: [
+    ...ROUTER_DIRECTIVES, ...COMMON_DIRECTIVES,
+    NavigationComponent, OffcanvasComponent,
+    LoginComponent, FooterComponent,
+    LoadingComponent
+  ],
   pipes: [],
   host: {},
   styleUrls: [
@@ -33,11 +41,11 @@ export class App {
 
   states: States;
 
-  constructor(private _AppStates: AppStates) {
+  constructor (private _AppStates: AppStates) {
     this.states = this._AppStates.states;
   }
 
-  toggleOffcanvas() {
+  toggleOffcanvas () {
     this._AppStates.toggleOffcanvas();
   }
 }
