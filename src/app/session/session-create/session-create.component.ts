8dd00a03e4eeb9b1df87167784bb2db15e6d1904
Sascha Nowak <sascha.nowak@netlogix.de>
2016-06-10 17:56:33 +0200

    [TASK] Add basic session create
    Missing:
    * Active user
    * Material Design components

--- src/app/session/session-create/session-create.component.ts
+++ src/app/session/session-create/session-create.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FORM_DIRECTIVES, Control, ControlGroup, FormBuilder, Validators } from '@angular/common';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { Markdown } from '../../markdown.pipe';
+import { SessionService } from '../../shared';
 
 @Component({
   moduleId: module.id,
   selector: 't3dd16-session-create',
+  directives: [ FORM_DIRECTIVES ],
+  providers: [ SessionService ],
+  pipes: [ Markdown ],
   templateUrl: 'session-create.component.html',
-  styleUrls: ['session-create.component.css']
+  styleUrls: [ 'session-create.component.css' ]
 })
-export class SessionCreateComponent implements OnInit {
+export class SessionCreateComponent {
 
-  constructor() {}
+  title: Control;
+  description: Control;
+  form: ControlGroup;
 
-  ngOnInit() {
+  constructor(private router: Router, private builder: FormBuilder, private sessionService: SessionService, protected _title: Title) {
+    this._title.setTitle('Propose ' + this._title.getTitle());
+    this.title = new Control('', Validators.required);
+    this.description = new Control('', Validators.required);
+    this.form = builder.group({
+      title: this.title,
+      description: this.description
+    });
+  }
+
+  close() {
+    this._title.setTitle(this._title.getTitle().substring(8));
+    this.router.navigateByUrl('/sessions');
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.sessionService.create(this.form.value).subscribe(
+        res => this.close(),
+        error => console.log(error)
+      );
+    }
   }
 
 }
