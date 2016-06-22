333e4f5380d764aa6e44c42a2c25f326b4a93337
Sascha Nowak <sascha.nowak@netlogix.de>
2016-06-22 09:46:38 +0200

    [BUGFIX] Fix external links

--- src/app/content-link.directive.ts
+++ src/app/content-link.directive.ts
@@ -9,7 +9,7 @@ function isInternalLink(link: string): boolean {
   if (link.indexOf('mailto') !== -1) {
     return false;
   } else if (link.indexOf('http') !== -1) {
-    return true;
+    return false;
   }
   return true;
 }
@@ -22,12 +22,13 @@ function isInternalLink(link: string): boolean {
 })
 export class ContentLink {
   @Input() href: string;
+  @Input() target: string;
 
   constructor(private router: Router) {
   }
 
   onClick(): boolean {
-    if (isString(this.href) && isInternalLink(this.href)) {
+    if (this.target !== '_blank' && isString(this.href) && isInternalLink(this.href)) {
       this.router.navigateByUrl(this.href.replace (/^[a-z]{4,5}\:\/{2}[a-z]{1,}\:[0-9]{1,4}.(.*)/, '$1'));
       return false;
     }
