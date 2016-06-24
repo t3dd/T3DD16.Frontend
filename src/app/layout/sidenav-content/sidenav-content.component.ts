29758406e6e04a83a2a8e02bf96285903373d5ff
Sascha Nowak <sascha.nowak@netlogix.de>
2016-06-24 18:34:51 +0200

    [TASK] Close sidenav on click

--- src/app/layout/sidenav-content/sidenav-content.component.ts
+++ src/app/layout/sidenav-content/sidenav-content.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
+import { MdSidenav } from '@angular2-material/sidenav';
 
 import { ContentLink } from '../../content-link.directive';
 import { CmsService } from '../../shared';
@@ -15,6 +16,7 @@ import { Page } from '../../model/page';
 })
 export class SidenavContentComponent implements OnInit {
 
+  @Input() sidenav: MdSidenav;
   pages$: Observable<Page>;
 
   constructor(private cmsService: CmsService) {
